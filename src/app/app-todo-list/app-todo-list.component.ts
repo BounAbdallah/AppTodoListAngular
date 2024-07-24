7730d4d0763bf133b6d8ca43c5a445988586f66e
BounAbdallah <bigmabo61345@gmail.com>
2024-07-24 12:11:07 +0000

    Ajout de la composant app-to-do-list, j'ai ajouter sans initialiser git-flow par oublie

---
+++ src/app/app-todo-list/app-todo-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-todo-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-todo-list.component.html',
+  styleUrl: './app-todo-list.component.css'
+})
+export class AppTodoListComponent {
+
+}
